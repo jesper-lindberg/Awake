7e5ee47643e7dc3b08ffa8137edce5339598872a
Krystian Postek <krystian@postek.eu>
2021-06-20 12:00:41 +0200

    Format file

--- Tests/AwakeTests/AwakeTests.swift
+++ Tests/AwakeTests/AwakeTests.swift
@@ -1,10 +1,10 @@
-    import XCTest
-    @testable import Awake
+@testable import Awake
+import XCTest
 
-    final class AwakeTests: XCTestCase {
-        func wakeMyMachine() {
-            // IMPORTANT! replace mac addr with yours, and in terminal run this command 'nc -ul 9'
-            let device = Awake.Device(MAC: "18:c0:4d:a2:ba:8a", BroadcastAddr: "255.255.255.255")
-            let _ = Awake.target(device: device)
-        }
+final class AwakeTests: XCTestCase {
+    func wakeMyMachine() {
+        // IMPORTANT! replace mac addr with yours, and in terminal run this command 'nc -ul 9'
+        let device = Awake.Device(MAC: "18:c0:4d:a2:ba:8a", BroadcastAddr: "255.255.255.255")
+        _ = Awake.target(device: device)
     }
+}
